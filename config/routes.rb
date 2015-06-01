02b165454b28fb8bd36606c13289bb061ee6fc61
Patrick Jones <pmichaeljones@gmail.com>
2015-06-01 12:59:23 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#front'
 
   get 'about-chris', to: 'pages#about-chris', as: 'about_chris'
+  get 'faq', to: 'pages#faq'
+  get 'important-information', to: 'pages#important-info'
+  get 'sponsorship', to: 'pages#sponsor-info'
+  get 'make-donation', to: 'pages#donation-info'
+  get 'volunteer', to: 'pages#volunteer-info'
 
 end
