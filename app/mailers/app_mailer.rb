6dfe92b6f15562e9b6a33262719ff01e615a6321
Patrick Jones <pmichaeljones@gmail.com>
2015-06-01 14:27:41 -0600

    added app mailer

---
+++ app/mailers/app_mailer.rb
@@ -0,0 +1,19 @@
+class AppMailer < ActionMailer::Base
+
+  def copilot_contact_form(params)
+
+  end
+
+  def volunteer_form(params)
+
+  end
+
+  def sponsor_form(params)
+
+  end
+
+  def general_contact(params)
+
+  end
+
+end
