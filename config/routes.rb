ec1c2fb43d92ca2550680802b2ae7cf6b6336219
Patrick Jones <pmichaeljones@gmail.com>
2015-06-01 14:50:41 -0600

    added sponsorship page

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,17 @@ Rails.application.routes.draw do
   get 'about-chris', to: 'pages#about-chris', as: 'about_chris'
   get 'faq', to: 'pages#faq'
   get 'important-information', to: 'pages#important-info'
-  get 'sponsorship', to: 'pages#sponsor-info'
+
   get 'make-donation', to: 'pages#donation-info'
+
+  get 'sponsorship', to: 'pages#sponsor-info'
+  post 'sponsorship_form', to: 'notifications#sponsorship'
+
   get 'volunteer', to: 'pages#volunteer-info'
+  post 'volunteer_form', to: 'notifications#volunteer'
+
+  post 'copilot_form', to: 'notifications#copilot'
+
+
 
 end
