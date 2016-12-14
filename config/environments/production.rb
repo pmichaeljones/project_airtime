8fe763b08a1dc5f547419bd0e412a15bc778d53e
Patrick Jones <pmichaeljones@gmail.com>
2016-12-14 16:18:05 -0300

    new GMAIL variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,11 +15,10 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'project-airtime.herokuapp.com',
+  :port           => 587,
+  :address        => "smtp.gmail.com",
+  :user_name      => ENV['GMAIL_SMTP_USER'],
+  :password       => ENV['GMAIL_SMTP_PASSWORD'],
   :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
