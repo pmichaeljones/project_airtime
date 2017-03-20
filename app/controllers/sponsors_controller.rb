35ee28204d1c068346c9d7373cd88317dde593db
Patrick Jones <pmichaeljones@gmail.com>
2017-03-20 19:10:46 +0100

    finish sponsor portion

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,11 +1,15 @@
 class SponsorsController < ApplicationController
 
 	def create
-	
+   new_sponsor = Sponsor.new(name: params[:sponsor])
+   new_sponsor.save
+   redirect_to dashboard_path
 	end
 
 	def destroy
-	
+    sponsor = Sponsor.find(params[:id])
+    sponsor.delete
+    redirect_to dashboard_path
 	end
 
 end
