603eac386314a169cd73dd4b2c78a359d31d782c
Patrick Jones <pmichaeljones@gmail.com>
2017-03-20 14:05:03 +0100

    new mailgun domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'project-airtime.heroku.com',
+  :domain         => ENV['MAILGUN_DOMAIN'],
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
